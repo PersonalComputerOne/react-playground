c898768651bcca65307257ead173950d4d911d99
John Marvin M. Ramos <jm.ramos00569@student.tsu.edu.ph>
2025-03-03 19:29:53 +0800

    Add files via upload

---
+++ shopping-list/App.jsx
@@ -0,0 +1,19 @@
+import ShoppingList from './ShoppingList';
+import './App.css';
+
+const data = [
+  {item: "eggs", quantity: 12, completed: false},
+  {item: "milks", quantity: 2, completed: true},
+  {item: "breads", quantity: 1, completed: false},
+  {item: "bacons", quantity: 22, completed: true},
+]
+
+function App() {
+  return (
+    <>
+      <ShoppingList items={data}/>
+    </>
+  )
+}
+
+export default App
